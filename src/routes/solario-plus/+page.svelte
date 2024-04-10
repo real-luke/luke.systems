7d97971cfc94c60e3f5302c8e3b0e1c68725aef3
Luke <lukesturgill922@gmail.com>
2024-04-09 23:15:23 -0400

    Add Solario+ Page

---
+++ src/routes/solario-plus/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import logo from '$lib/static/solario-plus.png';
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<div class="space-y-2 text-surface-300">
+			<div class="flex justify-center space-x-2">
+				<img src={logo} alt="Solario+">
+				<a href="/about" class="btn variant-filled-error">Download Solario+</a>
+			</div>
+		</div>
+	</div>
+</div>
