609dae4c6c4ed72bb3e131e0049134050666438e
Luke <lukesturgill922@gmail.com>
2024-04-10 13:31:29 -0400

    Add Solario+ Download

--- src/routes/solario-plus/+page.svelte
+++ src/routes/solario-plus/+page.svelte
@@ -1,10 +1,12 @@
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
+		<img src="/solario-plus.png" alt="Solario+"/>
+		<h1 class="h2">Solario+</h1>
+		<div class="flex justify-center space-x-2">
+			<a href="https://cdn.discordapp.com/attachments/1225630587956760589/1227671619669856309/Solario.crx?ex=6629414b&amp;is=6616cc4b&amp;hm=aa0cb37f72e752b8b333147f7e7917818144c445e3eb53f20251347a081850db&amp;" rel="noreferrer noopener" target="_blank" class="btn variant-filled-error">Download</a>
+		</div>
 		<div class="space-y-2 text-surface-300">
-			<div class="flex justify-center space-x-2">
-				<img src="/solario-plus.png" alt="Solario+">
-				<a href="/about" class="btn variant-filled-error">Download Solario+</a>
-			</div>
+			<p>Solario+ is still in early development.</p>
 		</div>
 	</div>
 </div>
